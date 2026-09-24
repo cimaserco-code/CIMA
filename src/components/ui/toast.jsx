9481059fa472b276277ec8a8e456b76e5db19c9b
Emmanuel Castro <emmanuelcastro.serco@gmail.com>
2026-09-24 12:10:09 -0600

    pop up toaster

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -63,11 +63,24 @@ ToastAction.displayName = "ToastAction";
 const ToastClose = React.forwardRef(({ className, ...props }, ref) => (
   <button
     ref={ref}
+    type="button"
     className={cn(
       "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
       className
     )}
     toast-close=""
+    onClick={(event) => {
+      event.preventDefault();
+
+      const toastElement = event.currentTarget.closest("[data-toast]");
+      const closeEvent = new CustomEvent("toast-close", {
+        bubbles: true,
+      });
+
+      if (toastElement) {
+        toastElement.dispatchEvent(closeEvent);
+      }
+    }}
     {...props}
   >
     <X className="h-4 w-4" />
--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -15,7 +15,11 @@ export function Toaster() {
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast
+  key={id}
+  {...props}
+  data-toast
+>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
@@ -23,7 +27,7 @@ export function Toaster() {
               )}
             </div>
             {action}
-            <ToastClose />
+            <ToastClose onClick={() => props.onOpenChange?.(false)} />
           </Toast>
         );
       })}
