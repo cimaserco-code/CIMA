2578592e3c53c63bac49a2f1db106eb695e22f63
Emmanuel Castro <emmanuelcastro.serco@gmail.com>
2026-09-24 12:19:16 -0600

    toast

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -2,36 +2,35 @@ import { useToast } from "@/components/ui/use-toast";
 import {
   Toast,
   ToastClose,
-  ToastDescription,
   ToastProvider,
+  ToastDescription,
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast";
 
 export function Toaster() {
-  const { toasts } = useToast();
+  const { toasts, dismiss } = useToast();
 
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast
-  key={id}
-  {...props}
-  data-toast
->
+          <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
                 <ToastDescription>{description}</ToastDescription>
               )}
             </div>
+
             {action}
-            <ToastClose onClick={() => props.onOpenChange?.(false)} />
+
+            <ToastClose onClick={() => dismiss(id)} />
           </Toast>
         );
       })}
+
       <ToastViewport />
     </ToastProvider>
   );
-} 
+}
